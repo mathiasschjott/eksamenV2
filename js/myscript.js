460c712f8d6f683683baa6fd626c5814cca03fb1
Mathias H. Schjøtt <math881r@edu.campusvejle.dk>
2016-01-13 13:06:25 +0100

    Små rettelser af stave fejl.

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$('.fadein').hide().fadeIn(2000);
 	
-	$('.SeeMore2').click(function(){
-		$(this).toggleClass('SeeMore2');
-		if($(this).hasClass('SeeMore2')){
+	$('.seeMore').click(function(){
+		$(this).toggleClass('seeMore');
+		if($(this).hasClass('seeMore')){
 			$(this).text('Se mere');			
 		} else {
 			$(this).text('Se mindre');
